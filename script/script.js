f440f6c847cd0603e9bbb4dc88afa1c0fefe1fdb
thiago-dalto <tmonteirodalto@icloud.com>
2024-04-10 12:06:13 -0300

    login'

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aqui você pode adicionar o código para enviar os dados do formulário para a autenticação
+});
+
+document.getElementById('register-btn').addEventListener('click', function() {
+    // Aqui você pode adicionar o código para redirecionar para a página de registro
+    alert('Redirecionar para a página de registro');
+});
