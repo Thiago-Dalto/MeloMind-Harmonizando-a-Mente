1fd3f1dc325086c284bcec91173271ef2fd4d1a4
thiago-dalto <tmonteirodalto@icloud.com>
2024-04-22 10:35:46 -0300

    json

--- js/rep-music.js
+++ js/rep-music.js
@@ -1,25 +1,44 @@
-const token = 'YOUR_ACCESS_TOKEN'; // Você obterá isso via OAuth
-const trackNameElement = document.getElementById('trackName');
+// script.js
+let currentTrack = 0;
+let tracks = [];
 
-function play() {
-    fetch('https://api.spotify.com/v1/me/player/play', {
-        method: 'PUT',
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
+function loadMusic() {
+    fetch('teste.json')
+        .then(response => response.json())
+        .then(data => {
+            tracks = data;
+            playTrack();
+        });
 }
 
-function pause() {
-    fetch('https://api.spotify.com/v1/me/player/pause', {
-        method: 'PUT',
-        headers: { 'Authorization': 'Bearer ' + token }
-    });
+function playTrack() {
+    if (tracks.length > 0) {
+        const player = document.getElementById('audioPlayer');
+        const trackTitle = document.getElementById('trackTitle');
+        player.src = tracks[currentTrack].file;
+        trackTitle.textContent = `Tocando: ${tracks[currentTrack].title}`;
+        player.play();
+    }
 }
-document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const feedback = document.getElementById('feedbackInput').value;
-    console.log('Feedback recebido:', feedback);
-    // Aqui você pode adicionar código para enviar o feedback ao servidor
-    // Limpa o campo de input após o envio
-    document.getElementById('feedbackInput').value = '';
-    alert('Obrigado pelo seu feedback!');
-});
+
+function nextTrack() {
+    if (currentTrack < tracks.length - 1) {
+        currentTrack++;
+        playTrack();
+    } else {
+        currentTrack = 0;  // Voltar ao início da lista
+        playTrack();
+    }
+}
+
+function previousTrack() {
+    if (currentTrack > 0) {
+        currentTrack--;
+        playTrack();
+    } else {
+        currentTrack = tracks.length - 1;  // Ir para a última faixa
+        playTrack();
+    }
+}
+
+window.onload = loadMusic;
